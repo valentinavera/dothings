2350809c0f2464194b624d475fd8849d683e216e
Valentina <valentinav@unicauca.edu.co>
2020-10-26 00:44:04 -0500

    task adapter listener and new modal R.layout.fragment_modal

--- app/src/main/java/edu/unicauca/main/TaskAdapter.java
+++ app/src/main/java/edu/unicauca/main/TaskAdapter.java
@@ -1,5 +1,6 @@
 package edu.unicauca.main;
 
+import android.text.style.AlignmentSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,40 +13,56 @@ import java.util.List;
 
 import edu.unicauca.main.models.TaskModel;
 
-public class TaskAdapter extends RecyclerView.Adapter <TaskAdapter.ViewHolder> {
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.ViewHolder> implements View.OnClickListener{
     private int resource;
     private List<TaskModel> taskList;
-    public TaskAdapter(List<TaskModel> taskList, int resource){
-        this.taskList=taskList;
+    private View.OnClickListener Listener;
+
+    public TaskAdapter(List<TaskModel> taskList, int resource) {
+        this.taskList = taskList;
         this.resource = resource;
 
     }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext ()).inflate (resource, parent, false);
-        return new ViewHolder(view);
+        View view = LayoutInflater.from(parent.getContext()).inflate(resource, parent, false);
+        ViewHolder vista = new ViewHolder(view);
+        view.setOnClickListener(this);
+        return vista;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-      TaskModel objTask = taskList.get (position);
-      holder.textViewTarea.setText (objTask.getName());
+        TaskModel objTask = taskList.get(position);
+        holder.textViewTarea.setText(objTask.getName());
     }
 
     @Override
     public int getItemCount() {
-        return taskList.size ();
+        return taskList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public void setOnClickListen(View.OnClickListener clickListener){
+        this.Listener = clickListener;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(this.Listener!=null){
+            Listener.onClick(v);
+        }
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
         private TextView textViewTarea;
         public View view;
 
-        public ViewHolder(View view){
+        public ViewHolder(View view) {
             super(view);
-            this.view=view;
-            this.textViewTarea= (TextView) view.findViewById (R.id.textViewTareas);
+            this.view = view;
+            this.textViewTarea = (TextView) view.findViewById(R.id.textViewTareas);
         }
     }
 
